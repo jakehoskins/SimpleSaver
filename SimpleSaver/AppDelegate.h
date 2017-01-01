9491043079878586994094e7c1f83ab9af6b6550
Jake Hoskins <jakehoskins95@googlemail.com>
2017-01-01 14:01:54 +0000

    init

---
+++ SimpleSaver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleSaver
+//
+//  Created by Jake Hoskins on 01/01/2017.
+//  Copyright © 2017 Jake Hoskins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleSaver/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleSaver
+//
+//  Created by Jake Hoskins on 01/01/2017.
+//  Copyright © 2017 Jake Hoskins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
